0e09c9b64ee6060e444930bc3073d0e73c0fb47e
YiYidecat <1395947873@qq.com>
2025-07-13 20:40:40 +0800

    静态资源的引入

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+//引入初始化样式文件
+import "@/styles/common.scss"
+
 
 //测试接口函数
 import { getCategory } from '@/apis/testAPI'
