bda2e44aa26ededf475e8c60b02bf0ca77d1b221
YiYidecat <1395947873@qq.com>
2025-07-13 21:21:58 +0800

    scss自动导入配置

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,18 @@
 <template>
   <!-- 一级路由出口组件 -->
    <RouterView></RouterView>
-   
+   <div class="test">
+    test css
+   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+
+.test{
+  color:$priceColor
+}
+
+
 header {
   line-height: 1.5;
   max-height: 100vh;
